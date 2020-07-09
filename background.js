8b7012f99822ab2484d27f0a4ae933da906984ba
q-fukutomo <katatsuki52@gmail.com>
2020-07-10 08:54:29 +0900

    修正: 閉じるボタン関係の処理

--- background.js
+++ background.js
@@ -12,3 +12,12 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
+
+// content側で停止押したときの処理
+chrome.runtime.onMessage.addListener(
+    function(request,sender,sendResponse){
+        chrome.storage.local.set({'executeOperation':'start'},function(){});
+        sendResponse({status: "OK"});
+        return true;
+    }
+);
