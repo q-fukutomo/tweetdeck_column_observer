902bcfb5ad766306fcfc799df708be1a998f2cd3
q-fukutomo <katatsuki52@gmail.com>
2022-02-22 22:18:23 +0000

    使わなくなったファイルの削除

--- bin/get-ext-version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const jsonFilePath = path.resolve(__dirname, '../src/manifest.json');
-const jsonObject = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
-const { version } = jsonObject;
-
-console.log(version);
